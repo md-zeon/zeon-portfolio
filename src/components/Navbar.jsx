45397f66c682796f9d5bf93276b824c9d1518d8c
md-zeon <zeon.cse@gmail.com>
2026-08-29 13:38:49 +0600

    fix: make archive banner an in-flow top bar so it no longer covers the nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
 	];
 
 	return (
-		<nav className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+		<nav className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 			<div className='flex justify-between items-center h-16'>
 				{/* Logo */}
 				<motion.div
